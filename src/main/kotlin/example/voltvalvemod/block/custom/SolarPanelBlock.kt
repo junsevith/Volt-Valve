40eb54144c7ad2833c51e61261db0aa11d6cddb2
JarJarIsMyLord <konczu.03@wp.pl>
2024-12-12 12:50:13 +0100

    First steps

---
+++ src/main/kotlin/example/voltvalvemod/block/custom/SolarPanelBlock.kt
@@ -0,0 +1,14 @@
+package example.voltvalvemod.block.custom
+
+import example.voltvalvemod.block.Generator
+import net.minecraft.world.level.block.Block
+
+class SolarPanelBlock(pProperties: Properties) : Block(pProperties), Generator {
+    override fun getVoltage(): Double {
+        TODO("Not yet implemented")
+    }
+
+    override fun getInnerResistance(): Double {
+        TODO("Not yet implemented")
+    }
+}
