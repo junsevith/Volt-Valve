0dd82d66ca2d1506217d75e3601cf1a649bce38a
JarJarIsMyLord <konczu.03@wp.pl>
2024-12-30 00:33:23 +0100

    Deleted unused imports.

--- src/main/kotlin/example/voltvalvemod/block/custom/SolarPanelBlock.kt
+++ src/main/kotlin/example/voltvalvemod/block/custom/SolarPanelBlock.kt
@@ -1,8 +1,6 @@
 package example.voltvalvemod.block.custom
 
 import example.voltvalvemod.VoltValveMod
-import example.voltvalvemod.block.entity.CableEntity
-import example.voltvalvemod.block.entity.ModBlockEntities
 import example.voltvalvemod.block.entity.SolarPanelBlockEntity
 import net.minecraft.core.BlockPos
 import net.minecraft.world.InteractionHand
@@ -13,13 +11,10 @@ import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.BaseEntityBlock
 import net.minecraft.world.level.block.RenderShape
 import net.minecraft.world.level.block.entity.BlockEntity
-import net.minecraft.world.level.block.entity.BlockEntityTicker
-import net.minecraft.world.level.block.entity.BlockEntityType
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.phys.BlockHitResult
 import net.minecraft.world.phys.shapes.CollisionContext
 import net.minecraft.world.phys.shapes.VoxelShape
-import java.awt.Panel
 
 class SolarPanelBlock(pProperties: Properties) : BaseEntityBlock(pProperties) {
     private val SHAPE = box(0.0, 0.0, 0.0, 16.0, 6.0, 16.0)
