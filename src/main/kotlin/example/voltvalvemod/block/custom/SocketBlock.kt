752c31941b48d13e1e756c0f2e78ee67b4348e54
JarJarIsMyLord <konczu.03@wp.pl>
2024-12-30 20:33:17 +0100

    Added functionality to the socket block.

--- src/main/kotlin/example/voltvalvemod/block/custom/SocketBlock.kt
+++ src/main/kotlin/example/voltvalvemod/block/custom/SocketBlock.kt
@@ -1,17 +1,24 @@
 package example.voltvalvemod.block.custom
 
+import example.voltvalvemod.block.entity.SocketBlockEntity
+import net.minecraft.core.BlockPos
 import net.minecraft.world.item.context.BlockPlaceContext
 import net.minecraft.world.level.block.*
+import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.level.block.state.StateDefinition
 import net.minecraft.world.level.block.state.properties.Property
 
-class SocketBlock(pProperties: Properties) : DirectionalBlock(pProperties){
+class SocketBlock(pProperties: Properties) : DirectionalBlock(pProperties), EntityBlock{
     override fun getStateForPlacement(pContext: BlockPlaceContext): BlockState {
         return defaultBlockState().setValue(FACING, pContext.nearestLookingDirection.opposite) as BlockState
     }
 
     override fun createBlockStateDefinition(pBuilder: StateDefinition.Builder<Block?, BlockState?>) {
         pBuilder.add(*arrayOf<Property<*>>(FACING))
+    }
+
+    override fun newBlockEntity(pPos: BlockPos, pState: BlockState): BlockEntity {
+        return SocketBlockEntity(pPos, pState)
     }
 }
--- src/main/kotlin/example/voltvalvemod/block/entity/SocketBlockEntity.kt
+++ src/main/kotlin/example/voltvalvemod/block/entity/SocketBlockEntity.kt
@@ -1,30 +1,48 @@
 package example.voltvalvemod.block.entity
 
+import example.voltvalvemod.block.custom.PanelGrid
 import example.voltvalvemod.block.custom.PowerGrid
 import example.voltvalvemod.block.interfaces.Generator
+import example.voltvalvemod.block.interfaces.Transmitter
 import net.minecraft.core.BlockPos
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.world.level.block.state.properties.BlockStateProperties
 
 class SocketBlockEntity(pPos: BlockPos, pBlockState: BlockState) :
     BlockEntity(ModBlockEntities.SOCKET_BE.get(), pPos, pBlockState), Generator {
 
+    private val dir = this.blockState.getValue(BlockStateProperties.FACING)
+
     override var powerGrid: PowerGrid? = null
         set(value) {
-            field?.removeGenerator(this)
-            field = value
-            field?.updateGenerator(this)
+            val entity = this.level!!.getBlockEntity(this.blockPos.relative(dir))
+            if (entity is Transmitter && entity.powerGrid == value) {
+                field?.removeGenerator(this)
+                field = value
+                field?.updateGenerator(this)
+            }
         }
 
-
     override fun providePower(): Long {
-//        TODO("not implemented yet")
-        return 10
+        val neighbours = mutableListOf<BlockEntity?>()
+        neighbours.add(this.level!!.getBlockEntity(this.blockPos.north()))
+        neighbours.add(this.level!!.getBlockEntity(this.blockPos.south()))
+        neighbours.add(this.level!!.getBlockEntity(this.blockPos.west()))
+        neighbours.add(this.level!!.getBlockEntity(this.blockPos.east()))
+        neighbours.add(this.level!!.getBlockEntity(this.blockPos.above()))
+        neighbours.remove(this.level!!.getBlockEntity(this.blockPos.relative(dir)))
+
+        val grids = neighbours.filterIsInstance<SolarPanelBlockEntity>()
+            .map { panel -> panel.grid }
+            .toSet()
+
+        val sum = grids.sumOf(PanelGrid::providePowerSum)
+
+        return sum.toLong()
     }
 
     override fun isOn(): Boolean {
         return true
     }
-
-
 }
